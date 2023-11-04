d648805656b759d6277bb4efce5b981a588761d9
Steven Silvester <steven.silvester@ieee.org>
2023-11-04 11:17:28 -0500

    Adopt sp-repo-review

--- tests/test_comm.py
+++ tests/test_comm.py
@@ -1,16 +1,17 @@
+from typing import Any
 from comm.base_comm import BaseComm, CommManager
 
 
 class MyComm(BaseComm):
-    def publish_msg(self, msg_type, data=None, metadata=None, buffers=None, **keys):
+    def publish_msg(self, msg_type:str, data:Any=None, metadata:Any=None, buffers:Any=None, **keys:Any) ->None:
         pass
 
 
-def test_comm_manager():
+def test_comm_manager() -> None:
     test = CommManager()
     assert test.targets == {}
 
 
-def test_base_comm():
+def test_base_comm() -> None:
     test = MyComm()
     assert test.target_name == "comm"
